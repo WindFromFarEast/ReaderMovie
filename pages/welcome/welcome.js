ff6d04ac35392c93f3953dd0ce7fd727bf5a6580
WindFromFarEast <fromfareastqaq@gmail.com>
2018-02-26 12:19:30 +0800

    增加自动获取用户信息的功能

--- pages/welcome/welcome.js
+++ pages/welcome/welcome.js
@@ -4,14 +4,24 @@ Page({
      * 页面的初始数据
      */
     data: {
-
+        username: "",
     },
 
     /**
      * 生命周期函数--监听页面加载
      */
     onLoad: function (options) {
-
+        var that = this;
+        //获取用户信息
+        wx.getUserInfo({
+            success:function (result) {
+                console.log(result);
+                var username = result.userInfo.nickName;
+                that.setData({
+                    username: username,
+                });
+            }
+        });
     },
 
     /*点击开启小程序之旅按钮后触发事件，跳转页面*/
